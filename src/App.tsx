76f630060083c8ac43b82478c117d6ee15926ce1
g100rabh <gsaurabh909@gmai.com>
2024-04-18 00:30:02 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { wineData } from "./data/data";
+
+interface GroupedData {
+  [key: string]: {
+    Alcohol: number;
+    Ash: number | string; // Adjusted to allow both numbers and strings
+    Magnesium: number;
+    Flavanoids: string | number;
+    Proanthocyanins: string;
+    Hue: number;
+    Unknown: number;
+  }[];
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const groupedData: GroupedData = wineData.reduce(
+    (groups: GroupedData, wine) => {
+      // Extract the class number from the "Alcohol" property
+      const classNumber = `class${wine.Alcohol}`;
+
+      // Check if the group for the class exists, if not create it
+      if (!groups[classNumber]) {
+        groups[classNumber] = [];
+      }
+
+      const {
+        Alcohol,
+        Ash,
+        Magnesium,
+        Flavanoids,
+        Proanthocyanins,
+        Hue,
+        Unknown,
+      } = wine;
+
+      // Add the wine data to the corresponding group
+      groups[classNumber].push({
+        Alcohol,
+        Ash,
+        Magnesium,
+        Flavanoids,
+        Proanthocyanins,
+        Hue,
+        Unknown,
+      });
+
+      return groups;
+    },
+    {}
   );
+
+  console.log(groupedData);
+  return <>App</>;
 }
 
 export default App;
